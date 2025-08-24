e19c34418057141e7578cfd0ff8889779104048a
Andrey Rassokhin <ambruciy@gmail.com>
2025-08-24 17:02:39 +0300

    Add headles

--- conftest.py
+++ conftest.py
@@ -3,17 +3,19 @@ from selenium import webdriver
 from pages.create_account_page import CreateAccount
 from pages.eco_friendly_pages import EcoFriendly
 from pages.sale_page import SalePage
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
+import allure
+from allure_commons.types import AttachmentType
 
 
 @pytest.fixture(scope='function')
 def driver():
-    # options = Options()
-    # options.add_argument('--headless')
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('window-size=1920,1080')
+    driver = webdriver.Chrome(options=options)
     yield driver
+    allure.attach(driver.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType)
     driver.quit()
 
 
